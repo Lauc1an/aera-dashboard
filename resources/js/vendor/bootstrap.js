91b59a39f27fa2200fda82030e11c432fd4c490a
Lauc1an <marlomp@hotmail.com>
2022-05-23 18:48:58 -0500

    Modify node packages with Bootstrap 4

---
+++ resources/js/vendor/bootstrap.js
@@ -0,0 +1,27 @@
+import Alert from 'bootstrap/js/src/alert.js'
+import Button from 'bootstrap/js/src/button.js'
+import Carousel from 'bootstrap/js/src/carousel.js'
+import Collapse from 'bootstrap/js/src/collapse.js'
+import Dropdown from 'bootstrap/js/src/dropdown.js'
+import Modal from 'bootstrap/js/src/modal.js'
+import Popover from 'bootstrap/js/src/popover.js'
+import Scrollspy from 'bootstrap/js/src/scrollspy.js'
+import Tab from 'bootstrap/js/src/tab.js'
+import Toast from 'bootstrap/js/src/toast.js'
+import Tooltip from 'bootstrap/js/src/tooltip.js'
+import Util from 'bootstrap/js/src/util.js'
+
+export {
+    Util,
+    Alert,
+    Button,
+    Carousel,
+    Collapse,
+    Dropdown,
+    Modal,
+    Popover,
+    Scrollspy,
+    Tab,
+    Toast,
+    Tooltip
+}
