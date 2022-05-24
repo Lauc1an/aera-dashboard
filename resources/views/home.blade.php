5d0d4aaeb4e524bc25ff38e9a90053c4ed02f537
Lauc1an <marlomp@hotmail.com>
2022-05-23 19:08:09 -0500

    Adding basic Home Controller and View with Layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Home</h1>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
