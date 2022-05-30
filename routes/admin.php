789928efb5651a21b2b1d18af1f0bd19dc9d505b
Lauc1an <marlomp@hotmail.com>
2022-05-30 00:44:21 -0500

    Installing Livewire and adding Home Page

--- routes/admin.php
+++ routes/admin.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Admin\Auth\AuthenticatedAdminController;
 use App\Http\Controllers\Admin\Auth\PasswordResetAdminController;
 use App\Http\Controllers\Admin\Auth\NewPasswordAdminController;
-use App\Http\Controllers\Admin\HomeAdminController;
+use App\Http\Livewire\Admin\Home;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest:admin')->prefix('admin')->group(function () {
@@ -13,13 +13,13 @@ Route::middleware('guest:admin')->prefix('admin')->group(function () {
 
     Route::get('forgot-password', [PasswordResetAdminController::class, 'create'])->name('admin.password.request');
     Route::post('forgot-password', [PasswordResetAdminController::class, 'store'])->name('admin.password.email');
-    
+
     Route::get('reset-password/{token}', [NewPasswordAdminController::class, 'create'])->name('admin.password.reset');
     Route::post('reset-password', [NewPasswordAdminController::class, 'store'])->name('admin.password.update');
 });
 
 Route::middleware('auth:admin')->prefix('admin')->group(function () {
-    Route::get('/', [HomeAdminController::class, 'index'])->name('admin.home');
+    Route::get('/', Home::class)->name('admin.home');
 
     Route::post('logout', [AuthenticatedAdminController::class, 'destroy'])->name('admin.logout');
 });
